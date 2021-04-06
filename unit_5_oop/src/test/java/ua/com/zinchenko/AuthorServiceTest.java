59b82f076f81a18e730ead664b457a4a871c1116
Artem Zinchenko <promakey76@gmail.com>
2021-04-06 13:40:21 +0300

    Complete unit_5

--- unit_5_oop/src/test/java/ua/com/zinchenko/AuthorServiceTest.java
+++ unit_5_oop/src/test/java/ua/com/zinchenko/AuthorServiceTest.java
@@ -1,5 +1,6 @@
 package ua.com.zinchenko;
 
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
@@ -14,18 +15,59 @@ import ua.com.zinchenko.service.impl.AuthorServiceImpl;
 
 public class AuthorServiceTest {
 
-    @Mock
-    private AuthorRepository authorRepository;
+    private final static AuthorService authorService = new AuthorServiceImpl(new AuthorRepositoryImpl());
+    private final static String authorName = "testName";
+    private final static String authorSurname = "testSurname";
 
-    private  final AuthorService authorService = new AuthorServiceImpl(authorRepository);
+    @After
+    public void clean() {
+        authorService.getAllAuthors().clear();
+    }
 
     @Test
-    public void create() {
-        Author testAuthor = new Author();
-        testAuthor.setName("Name1");
-        testAuthor.setSurname("Surname1");
-        authorService.createAuthor(testAuthor);
-        Author authorFromBd = authorService.getAuthorById(0);
-        Assert.assertEquals(testAuthor, authorFromBd);
+    public void createTest() {
+        authorService.createAuthor(new Author(authorName, authorSurname));
+        Assert.assertNotNull(authorService.getByName(authorName, authorSurname));
+    }
+
+    @Test
+    public void getByIdTest() {
+        Author author = new Author(authorName, authorSurname);
+        int authorId = author.getId();
+        authorService.createAuthor(author);
+        Assert.assertEquals(authorService.getAuthorById(authorId).getName(), author.getName());
+    }
+
+    @Test
+    public void getAllTest() {
+        for(int i = 0; i < 10; i++) {
+            authorService.createAuthor(new Author(authorName + i, authorSurname + i));
+        }
+        Assert.assertEquals(authorService.getAllAuthors().size(), 10);
+    }
+
+    @Test
+    public void deleteTest() {
+        for(int i = 0; i < 10; i++) {
+            authorService.createAuthor(new Author(authorName + i, authorSurname + i));
+        }
+        for(int i = 0; i < 5; i++) {
+            authorService.removeAuthor(authorService.getByName(authorName + i, authorSurname + i));
+        }
+        Assert.assertEquals(authorService.getAllAuthors().size(), 5);
+    }
+
+    @Test
+    public void updateTest() {
+        for(int i = 0; i < 10; i++) {
+            authorService.createAuthor(new Author(authorName + i, authorSurname + i));
+        }
+        Author author = authorService.getByName(authorName + 5, authorSurname + 5);
+        int authorId = author.getId();
+        author.setSurname(authorSurname + "new");
+        author.setName(authorName + "new");
+        authorService.updateAuthor(author);
+        Assert.assertEquals(authorService.getAuthorById(authorId).getName(), author.getName());
+        Assert.assertEquals(authorService.getAuthorById(authorId).getSurname(), author.getSurname());
     }
 }
--- unit_5_oop/src/test/java/ua/com/zinchenko/BookServiceTest.java
+++ unit_5_oop/src/test/java/ua/com/zinchenko/BookServiceTest.java
@@ -1,4 +1,69 @@
 package ua.com.zinchenko;
 
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Test;
+import ua.com.zinchenko.entity.Author;
+import ua.com.zinchenko.entity.Book;
+import ua.com.zinchenko.repository.impl.AuthorRepositoryImpl;
+import ua.com.zinchenko.repository.impl.BookRepositoryImpl;
+import ua.com.zinchenko.service.AuthorService;
+import ua.com.zinchenko.service.BookService;
+import ua.com.zinchenko.service.impl.AuthorServiceImpl;
+import ua.com.zinchenko.service.impl.BookServiceImpl;
+
 public class BookServiceTest {
+
+    private final static BookService bookService = new BookServiceImpl(new BookRepositoryImpl());
+    private final static String bookName = "testName";
+
+    @After
+    public void clean() {
+        bookService.getAllBooks().clear();
+    }
+
+    @Test
+    public void createTest() {
+        bookService.createBook(new Book(bookName));
+        Assert.assertNotNull(bookService.getByName(bookName));
+    }
+
+    @Test
+    public void getByIdTest() {
+        Book book = new Book(bookName);
+        int bookId = book.getId();
+        bookService.createBook(book);
+        Assert.assertEquals(bookService.getBookById(bookId).getName(), book.getName());
+    }
+
+    @Test
+    public void getAllTest() {
+        for(int i = 0; i < 10; i++) {
+            bookService.createBook(new Book(bookName + i));
+        }
+        Assert.assertEquals(bookService.getAllBooks().size(), 10);
+    }
+
+    @Test
+    public void deleteTest() {
+        for(int i = 0; i < 10; i++) {
+            bookService.createBook(new Book(bookName + i));
+        }
+        for(int i = 0; i < 5; i++) {
+            bookService.removeBook(bookService.getByName(bookName + i));
+        }
+        Assert.assertEquals(bookService.getAllBooks().size(), 5);
+    }
+
+    @Test
+    public void updateTest() {
+        for(int i = 0; i < 10; i++) {
+            bookService.createBook(new Book(bookName + i));
+        }
+        Book book = bookService.getByName(bookName + 5);
+        int bookId = book.getId();
+        book.setName(bookName + "new");
+        bookService.updateBook(book);
+        Assert.assertEquals(bookService.getBookById(bookId).getName(), book.getName());
+    }
 }
